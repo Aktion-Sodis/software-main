b2a6e04f10536c7f99c6a126b669ed7c4b8df4e2
Ahmet Polat <polat2000@gmail.com>
2021-12-01 16:59:19 +0100

    init vuex store & page view for entities

--- frontend/src/store/entitiesModule/index.js
+++ frontend/src/store/entitiesModule/index.js
@@ -1,3 +1,5 @@
+import recursiveMarker from "./utils";
+
 const entitiesModule = {
   namespaced: true,
   state: () => ({
@@ -8,19 +10,49 @@ const entitiesModule = {
     ],
     hierarchialData: [
       { entityId: 0, hierarchyId: 0, upperEntityId: null, name: "Aachen" },
+      { entityId: 4, hierarchyId: 0, upperEntityId: null, name: "Sinop" },
+      { entityId: 2, hierarchyId: 1, upperEntityId: 0, name: "Nizzaallee" },
       {
         entityId: 1,
         hierarchyId: 1,
         upperEntityId: 0,
         name: "Mies van der Rohe Straße",
       },
-      { entityId: 2, hierarchyId: 1, upperEntityId: 0, name: "Nizzaallee" },
+      {
+        entityId: 5,
+        hierarchyId: 1,
+        upperEntityId: 4,
+        name: "Atatürk Mahallesi",
+      },
       { entityId: 3, hierarchyId: 2, upperEntityId: 2, name: "Eine 4er WG" },
     ],
+    treeRoot: {
+      entityId: 0,
+      hierarchyId: 0,
+      upperEntityId: null,
+      name: "Aachen",
+    },
   }),
   getters: {
     getAllEntitiesOfHierarchyByHid: (state) => (hid) => {
-      return state.hierarchialData.filter((e) => e.hierarchyId === hid);
+      return state.hierarchialData
+        .filter((e) => e.hierarchyId === hid)
+        .sort((a, b) => a.entityId - b.entityId); // sort by entityId ascending
+    },
+    getEntityHasSiblingUnderIt: (state, getters) => (entityId, hid) => {
+      const allDescendantsOfTreeRoot = getters.getAllDescendantsOfTreeRoot;
+      const allDescendantsOfTreeRootAtHid = allDescendantsOfTreeRoot.filter(
+        (e) => e.hierarchyId === hid
+      );
+      console.log(allDescendantsOfTreeRootAtHid);
+      return (
+        entityId !==
+        Math.max(
+          ...allDescendantsOfTreeRootAtHid
+            .map((e) => e.entityId)
+            .sort((a, b) => a - b) // sort by entityId ascending
+        )
+      );
     },
     getHierarchialData: (state) => {
       return state.hierarchialData;
@@ -28,9 +60,39 @@ const entitiesModule = {
     getHierarchialStructure: (state) => {
       return state.hierarchialStructure;
     },
+    getAllDescendantsOfTreeRoot: (state) => {
+      const treeRoot = state.treeRoot;
+      let markArray = state.hierarchialData.map((e) => ({
+        ...e,
+        marked: false,
+      }));
+
+      return recursiveMarker(treeRoot, markArray);
+    },
+    getEntityIsDescendantOfTreeRoot: (state, getters) => (entityId) => {
+      return getters.getAllDescendantsOfTreeRoot.some(
+        (e) => e.entityId === entityId
+      );
+    },
+    getHasChildren: (state, getters) => (entityId) => {
+      return getters.getHierarchialData.some(
+        (e) => e.upperEntityId === entityId
+      );
+    },
+    getTreeRoot: (state) => {
+      return state.treeRoot;
+    },
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setTreeRoot: (state, payload) => {
+      state.treeRoot = payload;
+    },
+  },
+  actions: {
+    clickOnEntity: ({ commit }, payload) => {
+      commit("setTreeRoot", payload);
+    },
+  },
 };
 
 export default entitiesModule;
--- frontend/src/views/OrganizationStructure.vue
+++ frontend/src/views/OrganizationStructure.vue
@@ -1,30 +1,45 @@
 <template>
     <div>
         <h1>Organization Structure</h1>
-        <div style="min-width: 100%;" class="my-8 d-flex overflow-x-auto">
+        <div class="my-8 d-flex" style="overflow-x: scroll">
             <div
                 v-for="(hierarchy, index) in hierarchialStructure"
                 :key="hierarchy.hierarchyId"
-                class="column d-flex flex-column align-center px-6"
+                class="column d-flex flex-column align-center px-8"
                 :class="hierarchy.upperHierarchy === null || 'dotted-left-border'"
             >
-                <h4 style="width: 90%;">Level {{ index }}</h4>
-                <div class="column-content-wrapper">
-                    <v-hover
+                <h4 style="width: 100%">Level {{ index }}</h4>
+                <div class="d-flex flex-column" style="width: 100%">
+                    <div
                         v-for="entity in allEntitiesOfHierarchy(hierarchy.hierarchyId)"
                         :key="entity.entityId"
-                        v-slot="{ hover }"
+                        class="d-flex flex-column align-center my-8"
                     >
-                        <v-sheet
-                            class="mx-auto transition-swing grey lighten-5 rounded-xl my-8 pa-4 text-center d-flex flex-column justify-center align-center"
-                            :class="hover ? 'lighten-2' : ''"
-                            elevation="4"
-                            min-height="64"
-                            width="100%"
+                        <div
+                            v-if="entityIsDescendantOfTreeRoot(entity.entityId)"
+                            class="entity-connection-left-line"
                         >
-                            <p>{{ entity.name }}</p>
-                        </v-sheet>
-                    </v-hover>
+                            <div
+                                v-if="hasSiblingsUnderIt(entity.entityId, entity.hierarchyId)"
+                                class="entity-connection-vertical-line"
+                            ></div>
+                        </div>
+                        <div
+                            v-if="hasChildren(entity.entityId) && (entityIsDescendantOfTreeRoot(entity.entityId) || entity.entityId === treeRoot.entityId)"
+                            class="entity-connection-right-line"
+                        ></div>
+                        <v-hover v-slot="{ hover }">
+                            <v-sheet
+                                class="mx-auto transition-swing grey lighten-5 rounded-xl pa-4 text-center d-flex flex-column justify-center align-center"
+                                :class="hover ? 'lighten-2' : ''"
+                                elevation="4"
+                                style="width: 100%; height: 128px; cursor: pointer"
+                                @click="clickOnEntity(entity)"
+                            >
+                                <p>{{ entity.name }}</p>
+                            </v-sheet>
+                        </v-hover>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,35 +47,65 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
     name: "OrganizationStructure",
     data: () => ({
     }),
+    mounted() {
+    },
     computed: {
         ...mapGetters({
             allEntitiesOfHierarchy: "entities/getAllEntitiesOfHierarchyByHid",
             hierarchialData: "entities/getHierarchialData",
-            hierarchialStructure: "entities/getHierarchialStructure"
+            hierarchialStructure: "entities/getHierarchialStructure",
+            hasSiblingsUnderIt: "entities/getEntityHasSiblingUnderIt",
+            allDescendantsOfTreeRoot: "entities/getAllDescendantsOfTreeRoot",
+            entityIsDescendantOfTreeRoot: "entities/getEntityIsDescendantOfTreeRoot",
+            hasChildren: "entities/getHasChildren",
+            treeRoot: "entities/getTreeRoot",
         }),
+    },
+    methods: {
+        ...mapActions({
+            clickOnEntity: 'entities/clickOnEntity', // map `this.add()` to `this.$store.dispatch('increment')`
+        })
     }
 };
 </script>
 
 <style scoped>
 .column {
-    min-width: 30rem;
-    flex: auto;
+    width: 32rem;
 }
 
 .dotted-left-border {
     border-left: 4px rgb(0, 0, 0, 0.2) dotted;
 }
 
-.column-content-wrapper {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
+.entity-connection-left-line {
+    position: relative;
+    height: 3px;
+    background-color: orange;
+    width: 16px;
+    right: calc(16rem - 24px);
+    top: 64px;
+}
+
+.entity-connection-right-line {
+    position: relative;
+    height: 3px;
+    background-color: orange;
+    width: 50px;
+    left: calc(16rem - 8px);
+    top: 64px;
+}
+
+.entity-connection-vertical-line {
+    position: absolute;
+    height: 196px;
+    background-color: orange;
+    width: 3px;
 }
 </style>
