83a0fb3a76e7b58e4f1457bd8e2fab8adf3fab80
CoachBenedetto <benediktschindeleuni@gmail.com>
2022-01-12 19:44:26 +0100

    initialization finished; circular dependency open

--- mobile_app/lib/services/authentication.dart
+++ mobile_app/lib/services/authentication.dart
@@ -6,3 +6,6 @@
 /// - Log out (am besten Future<bool>) -> rückgabe true wenn erfolgreich
 /// - Aktuelle user id (Future<String>) -> wenn nicht eingeloggt null
 /// - login status (Future<LoginStatus>) -> Enum für LoginStatus erstellen
+/// - wir verwenden nicht das login ui von amplify zunächst; können wir uns allerdings gemeinsam mit den designern nochmal anschauen
+/// - bei login immer an device erinnern
+/// - bei logout device vergewssen
