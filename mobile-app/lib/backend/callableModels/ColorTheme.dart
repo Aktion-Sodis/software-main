de40d40099ade73c63875dc0d868a3fc0dad5b99
CoachBenedetto <benediktschindeleuni@gmail.com>
2022-02-27 00:16:36 +0100

    added first conversion methods

--- mobile-app/lib/backend/callableModels/ColorTheme.dart
+++ mobile-app/lib/backend/callableModels/ColorTheme.dart
@@ -1,3 +1,5 @@
+import 'package:mobile_app/models/ModelProvider.dart' as amp;
+
 class ColorTheme {
   String? highlight;
   String? secondaryHighlight;
@@ -13,4 +15,23 @@ class ColorTheme {
       this.backgroundTwoLight,
       this.backgroundOneDark,
       this.backgroundTwoDark});
+
+  ColorTheme.fromAmplifyModel(amp.ColorTheme colorTheme) {
+    highlight = colorTheme.highlight;
+    secondaryHighlight = colorTheme.secondaryHighlight;
+    backgroundOneLight = colorTheme.backgroundOneLight;
+    backgroundTwoLight = colorTheme.backgroundTwoLight;
+    backgroundOneDark = colorTheme.backgroundOneDark;
+    backgroundTwoDark = colorTheme.backgroundTwoDark;
+  }
+
+  amp.ColorTheme toAmplifyModel() {
+    return amp.ColorTheme(
+        highlight: highlight,
+        secondaryHighlight: secondaryHighlight,
+        backgroundOneLight: backgroundOneLight,
+        backgroundTwoLight: backgroundTwoLight,
+        backgroundOneDark: backgroundOneDark,
+        backgroundTwoDark: backgroundTwoDark);
+  }
 }
