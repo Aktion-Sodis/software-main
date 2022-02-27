4ef044fd013a5b5d81d8c79af62c187abb8192d7
CoachBenedetto <benediktschindeleuni@gmail.com>
2022-02-27 01:43:26 +0100

    Model export created

---
+++ mobile-app/lib/backend/repositories/UserRepository.dart
@@ -0,0 +1,12 @@
+import 'package:mobile_app/models/ModelProvider.dart' as amp;
+import 'package:mobile_app/backend/callableModels/CallableModels.dart';
+
+class UserRepository {
+  static Future<User?> userByID(String ID) async {
+    //todo: implement
+  }
+
+  static Future<List<User>> allUsers() async {
+    //todo: implement
+  }
+}
