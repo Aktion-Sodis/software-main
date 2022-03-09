df23d7888ae41c80963da5bd67cdaf11b559b3d8
CoachBenedetto <benediktschindeleuni@gmail.com>
2022-03-09 21:13:18 +0100

    added flutter models

--- mobile-app/lib/backend/callableModels/CustomData.dart
+++ mobile-app/lib/backend/callableModels/CustomData.dart
@@ -1,21 +1,28 @@
 import 'package:mobile_app/backend/callableModels/AppliedCustomData.dart';
+import 'package:mobile_app/backend/callableModels/MultiLanguageText.dart';
 import 'package:mobile_app/models/ModelProvider.dart' as amp;
 
 class CustomData {
   String? id;
-  late String name;
+  late MultiLanguageText name_ml;
   late CustomDataType type;
 
-  CustomData({required this.id, required this.name, required this.type});
+  String get name => name_ml.text;
+
+  set name(String name) => name_ml.text = name;
+
+  CustomData({required this.id, required this.name_ml, required this.type});
 
   CustomData.fromAmplifyModel(amp.CustomData customData) {
     id = customData.id;
-    name = customData.name;
+    name_ml = MultiLanguageText.fromAmplifyModel(customData.name);
     type = customDataTypeByAmplifyType(customData.type);
   }
 
   amp.CustomData toAmplifyModel() {
     return (amp.CustomData(
-        name: name, type: ampDataTypeByCustomDataType(type), id: id));
+        name: name_ml.toAmplifyModel(),
+        type: ampDataTypeByCustomDataType(type),
+        id: id));
   }
 }
