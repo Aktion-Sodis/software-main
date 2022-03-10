7e5a85958685f981278ee6d0033fa98ecc1fcac1
ChristophNetsch <56590412+ChristophNetsch@users.noreply.github.com>
2022-03-10 22:00:40 +0100

    set default filename with datetime if null

--- mobile-app/lib/backend/storage/storage_repository.dart
+++ mobile-app/lib/backend/storage/storage_repository.dart
@@ -3,12 +3,11 @@ import 'dart:io';
 import 'package:amplify_flutter/amplify_flutter.dart';
 
 class StorageRepository {
-  Future<String> uploadFile(File file) async {
+  Future<String> uploadFile(File file, String? fileName) async {
     try {
-      final fileName = DateTime.now().toIso8601String();
       final result = await Amplify.Storage.uploadFile(
         local: file,
-        key: fileName + '.jpg',
+        key: fileName ?? DateTime.now().toIso8601String() + '.jpg',
       );
       
       return result.key;
