db303021d2df71818983ce3b6605877426fb111f
moritzsalamon <Z$8_89gNhhjGC-f>
2022-03-21 19:26:30 +0100

    implemented all questin types

--- mobile-app/lib/frontend/pages/survey.dart
+++ mobile-app/lib/frontend/pages/survey.dart
@@ -5,6 +5,7 @@ import 'package:mobile_app/backend/callableModels/CallableModels.dart';
 import 'package:mobile_app/frontend/common_widgets.dart';
 import 'package:mobile_app/frontend/dependentsizes.dart';
 import 'package:mobile_app/frontend/strings.dart';
+import 'package:mobile_app/services/photo_capturing.dart';
 import '../../backend/callableModels/ExecutedSurvey.dart';
 import '../../backend/callableModels/Survey.dart';
 
@@ -25,9 +26,19 @@ class SurveyWidgetState extends State<SurveyWidget> {
   final PageController _inSurveyPageController = PageController();
   late final Image surveyImage;
 
+  Map<Question, QuestionAnswer> answers = {};
+  late List<Question> questions;
+
   @override
   void initState() {
     surveyImage = Image.asset('assets/test/demo_pic.jpg');
+    questions = widget.survey.questions.where((element) => !element.isFollowUpQuestion).toList();
+    _inSurveyPageController.addListener(() {setState(() {
+
+    });});
+    _pageController.addListener(() {setState(() {
+
+    });});
     super.initState();
   }
 
@@ -61,11 +72,14 @@ class SurveyWidgetState extends State<SurveyWidget> {
         SizedBox(
           height: defaultPadding(context),
         ),
-        AnimatedProgressBar(0.5),
+        AnimatedProgressBar(_inSurveyPageController.hasClients?_inSurveyPageController.page!=null?((_inSurveyPageController.page!.round()+1)/questions.length):0:0),
         SizedBox(
           height: defaultPadding(context),
         ),
-        addTaskWidget(context: context, surveyTitle: widget.survey.name, addTask: addTask),
+        addTaskWidget(
+            context: context,
+            surveyTitle: widget.survey.name,
+            addTask: addTask),
         SizedBox(
           height: defaultPadding(context),
         ),
@@ -80,9 +94,60 @@ class SurveyWidgetState extends State<SurveyWidget> {
         ),
         bottomRowWidget(
             context: context,
-            onGoBack: () {},
+            onGoBack: () {
+              if (_inSurveyPageController.page?.round() != null) {
+                int page = _inSurveyPageController.page!.round();
+                if (page > 0) {
+                  _inSurveyPageController.previousPage(
+                      duration: _pageSlideDuration, curve: _pageSlideCurve);
+                } else {
+                  _pageController.previousPage(
+                      duration: _pageSlideDuration, curve: _pageSlideCurve);
+                }
+              }
+            },
             onDismiss: () {},
-            onProceed: () {}),
+            onProceed: () {
+              if (_inSurveyPageController.page?.round() != null) {
+                Question currentQuestion = questions[_inSurveyPageController.page!.round()];
+                if (currentQuestion.type == QuestionType.MULTIPLECHOICE) {
+                  answers[currentQuestion] ??= QuestionAnswer(
+                      questionID: widget.survey.id!,
+                      date: DateTime.now(),
+                      type: currentQuestion.type);
+                }
+                if (answers[currentQuestion] != null) {
+
+                  //check for followUpQuestions
+                  if(currentQuestion.type == QuestionType.SINGLECHOICE){
+                    String? followUpID = answers[currentQuestion]!.questionOptions!.first.followUpQuestionID;
+                    if(followUpID != null){
+                      List<Question> followUpQuestions = widget.survey.questions.where((element) => element.id == followUpID).toList();
+                      if(followUpQuestions.isNotEmpty){
+                        setState(() {
+                          questions.insert(questions.indexOf(currentQuestion)+1, followUpQuestions.first);
+                        });
+                      }
+                    }
+                    questions.removeWhere((element) {
+                      bool result =  element.isFollowUpQuestion && element.id != followUpID;
+                      if(result){
+                        answers.remove(element);
+                      }
+                      return result;
+                    });
+                  }
+                  _inSurveyPageController.nextPage(
+                      duration: _pageSlideDuration, curve: _pageSlideCurve);
+                }
+
+                if (currentQuestion.type == QuestionType.AUDIO || currentQuestion.type == QuestionType.PICTURE){
+                  //TODO: connect answers properly
+                  _inSurveyPageController.nextPage(
+                      duration: _pageSlideDuration, curve: _pageSlideCurve);
+                }
+              }
+            }),
         SizedBox(
           height: defaultPadding(context),
         ),
@@ -92,11 +157,20 @@ class SurveyWidgetState extends State<SurveyWidget> {
 
   List<Widget> convertSurveyQuestionsToWidgetList(
       {required BuildContext context}) {
-    return widget.survey.questions.map((e) {
+    return questions.map((e) {
       switch (e.type) {
         case QuestionType.SINGLECHOICE:
           return scQuestionWidget(context: context, question: e);
+        case QuestionType.MULTIPLECHOICE:
+          return mcQuestionWidget(context: context, question: e);
+        case QuestionType.PICTURE:
+          return takePhotoQuestionWidget(context: context, question: e);
+        case QuestionType.AUDIO:
+          return takeAudioQuestionWidget(context: context, question: e);
+        case QuestionType.TEXT:
+          return textQuestionWidget(context: context, question: e);
         default:
+
           //TODO: Container löschen
           return Container();
           throw UnimplementedError();
@@ -104,30 +178,247 @@ class SurveyWidgetState extends State<SurveyWidget> {
     }).toList();
   }
 
-  Widget scQuestionWidget({required BuildContext context, required Question question}) {
+  Widget scQuestionWidget(
+      {required BuildContext context, required Question question}) {
     return ListView(
       shrinkWrap: true,
       children: [
         imageForQuestion(question: question),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        questionTitleWidget(question: question, context: context),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        ...List.generate(
+            question.questionOptions!.length,
+            (index) => MaterialButton(
+                  padding: EdgeInsets.zero,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: defaultPadding(context)),
+                    child: Row(
+                      children: [
+                        Radio<QuestionOption>(
+                          value: question.questionOptions![index],
+                          groupValue: answers[question]?.questionOptions!.first,
+                          onChanged: (a) {
+                            setState(() {
+                              if (answers[question] == null) {
+                                answers[question] = QuestionAnswer(
+                                    questionID: question.id!,
+                                    date: DateTime.now(),
+                                    type: question.type)
+                                  ..questionOptions = [
+                                    question.questionOptions![index]
+                                  ];
+                              } else {
+                                answers[question]!.questionOptions = [
+                                  question.questionOptions![index]
+                                ];
+                              }
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          width: defaultPadding(context),
+                        ),
+                        Text(question.questionOptions![index].text),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (answers[question] == null) {
+                        answers[question] = QuestionAnswer(
+                            questionID: question.id!,
+                            date: DateTime.now(),
+                            type: question.type)
+                          ..questionOptions = [
+                            question.questionOptions![index]
+                          ];
+                      } else {
+                        answers[question]!.questionOptions = [
+                          question.questionOptions![index]
+                        ];
+                      }
+                    });
+                  },
+                ))
       ],
     );
   }
 
-  static Widget questionTitleWidget({required Question question, required BuildContext context}){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget mcQuestionWidget(
+      {required BuildContext context, required Question question}) {
+    return ListView(
+      shrinkWrap: true,
       children: [
-        Column(
-          children: [
-            Text(question.text, style: Theme.of(context).textTheme.bodyText1,),
-            Text(resolveQuestionTypeDescriptionFromQuestion(question: question)),
-          ],
+        imageForQuestion(question: question),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        questionTitleWidget(question: question, context: context),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        ...List.generate(
+            question.questionOptions!.length,
+            (index) => MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if(answers[question]!=null){
+                        if(!answers[question]!.questionOptions!.contains(question.questionOptions![index])){
+                          answers[question]!.questionOptions!.add(question.questionOptions![index]);
+                        }else{
+                          answers[question]!.questionOptions!.remove(question.questionOptions![index]);
+                        }
+                      }else{
+                        answers[question] = QuestionAnswer(questionID: question.id!, date: DateTime.now(), type: question.type)..questionOptions = [question.questionOptions![index]];
+                      }
+                    });
+                  },
+              padding: EdgeInsets.zero,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
+                child: Row(
+                  children: [
+                    Icon(
+                        (){
+                          if(answers[question]!=null){
+                            if(answers[question]!.questionOptions!.contains(question.questionOptions![index])){
+                              return MdiIcons.checkboxMarked;
+                            }
+                          }
+                          return MdiIcons.checkboxBlankOutline;
+                        }.call()
+                    ),
+                    SizedBox(
+                      width: defaultPadding(context),
+                    ),
+                    Text(question.questionOptions![index].text),
+                  ],
+                ),
+              ),
+                )),
+      ],
+    );
+  }
+
+  Widget textQuestionWidget(
+      {required BuildContext context, required Question question}) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        imageForQuestion(question: question),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        questionTitleWidget(question: question, context: context),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
+          child: TextField(
+            maxLines: 5,
+            onChanged: (String result){
+              answers[question] = QuestionAnswer(questionID: question.id!, date: DateTime.now(), type: QuestionType.TEXT)..text=result;
+            },
+          ),
         ),
       ],
     );
   }
 
-  static Widget addTaskWidget({required BuildContext context, required String surveyTitle, required Function addTask}){
+  Widget takePhotoQuestionWidget({required BuildContext context, required Question question}){
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        imageForQuestion(question: question),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(question.text),
+              ),
+              getReadOutWidget(question: question),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: defaultPadding(context) *2,
+        ),
+        getTakePhotoWidget(question: question, callback: (){}, context: context),
+      ],
+    );
+  }
+
+  Widget takeAudioQuestionWidget({required BuildContext context, required Question question}){
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        imageForQuestion(question: question),
+        SizedBox(
+          height: defaultPadding(context),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(question.text),
+              ),
+              getReadOutWidget(question: question),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: defaultPadding(context) *2,
+        ),
+        getTakeAudioWidget(question: question, callback: (){}, context: context),
+      ],
+    );
+  }
+
+
+  static Widget questionTitleWidget(
+      {required Question question, required BuildContext context}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  question.text,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(resolveQuestionTypeDescriptionFromQuestion(
+                    question: question)),
+              ],
+            ),
+          ),
+          getReadOutWidget(question: question),
+        ],
+      ),
+    );
+  }
+
+  static Widget addTaskWidget(
+      {required BuildContext context,
+      required String surveyTitle,
+      required Function addTask}) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: defaultPadding(context)),
       child: Row(
@@ -139,7 +430,7 @@ class SurveyWidgetState extends State<SurveyWidget> {
           ),
           IconButton(
             icon: const Icon(MdiIcons.checkboxMarkedOutline),
-            onPressed: (){
+            onPressed: () {
               addTask();
             },
           ),
@@ -224,8 +515,9 @@ class SurveyWidgetState extends State<SurveyWidget> {
     );
   }
 
-  static String resolveQuestionTypeDescriptionFromQuestion({required Question question}){
-    switch (question.type){
+  static String resolveQuestionTypeDescriptionFromQuestion(
+      {required Question question}) {
+    switch (question.type) {
       case QuestionType.SINGLECHOICE:
         return singleChoiceTypeDescription;
       case QuestionType.MULTIPLECHOICE:
@@ -237,15 +529,47 @@ class SurveyWidgetState extends State<SurveyWidget> {
     }
   }
 
-  static Widget imageForQuestion({required Question question}){
+  static Widget getReadOutWidget({required Question question}) {
+    //TODO: add correct Widget From Bene, connect read out functionality
 
+    return Container(
+      child: const Icon(MdiIcons.speaker),
+    );
+  }
+
+  static Widget getTakePhotoWidget({required Question question, required VoidCallback callback, required BuildContext context}){
+    //TODO: connect with Bene
+
+    return MaterialButton(
+      onPressed: (){
+        CameraFunctionality.takePicture(context: context);
+      },
+      child: Container(
+        child: const Icon(MdiIcons.cameraRetake, size: 40,),
+      ),
+    );
+  }
+
+  static Widget getTakeAudioWidget({required Question question, required VoidCallback callback, required BuildContext context}){
+    //TODO: connect with Bene
+
+    return MaterialButton(
+      onPressed: (){
+
+      },
+      child: Container(
+        child: const Icon(MdiIcons.microphone, size: 40,),
+      ),
+    );
+  }
+
+  static Widget imageForQuestion({required Question question}) {
     //TODO: resolve Image from question
 
     return Image.asset('assets/test/demo_pic.jpg');
   }
 
-  Future<bool?> addTask()async{
-
+  Future<bool?> addTask() async {
     //TODO: connect addTask
 
     return false;
