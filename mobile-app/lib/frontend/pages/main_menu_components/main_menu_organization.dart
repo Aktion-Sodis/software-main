f5e0ab43c2db42b6daab106687ba0a0102943eb3
CoachBenedetto <benediktschindeleuni@gmail.com>
2022-03-21 03:14:23 +0100

    first frontend start

--- mobile-app/lib/frontend/pages/main_menu_components/main_menu_organization.dart
+++ mobile-app/lib/frontend/pages/main_menu_components/main_menu_organization.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile_app/backend/Blocs/inapp/inapp_bloc.dart';
+import 'package:mobile_app/backend/Blocs/organization_view/organization_view_bloc.dart';
+import 'package:mobile_app/backend/Blocs/organization_view/organization_view_state.dart';
 import 'package:mobile_app/backend/callableModels/CallableModels.dart';
+import 'package:mobile_app/backend/repositories/AppliedInterventionRepository.dart';
 import 'package:mobile_app/backend/repositories/EntityRepository.dart';
 import 'package:mobile_app/backend/repositories/SurveyRepository.dart';
 import 'package:mobile_app/frontend/components/buttons.dart';
+import 'package:mobile_app/frontend/components/loadingsign.dart';
 import 'package:mobile_app/frontend/dependentsizes.dart';
 
-class MainMenuOrganization extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MainMenuOrganizationState();
-  }
-}
+class MainMenuOrganization extends StatelessWidget {
+  Widget appBarWidget(BuildContext context,
+      EntitiesLoadedOrganizationViewState organizationViewState) {}
 
-class MainMenuOrganizationState extends State<MainMenuOrganization> {
+  Widget mainWidget(BuildContext context,
+      EntitiesLoadedOrganizationViewState organizationViewState) {}
+
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
@@ -22,37 +26,33 @@ class MainMenuOrganizationState extends State<MainMenuOrganization> {
           RepositoryProvider<EntityRepository>(
               create: (context) => EntityRepository()),
           RepositoryProvider<SurveyRepository>(
-              create: (context) => SurveyRepository())
+              create: (context) => SurveyRepository()),
+          RepositoryProvider<EntityRepository>(
+              create: (context) => EntityRepository()),
+          RepositoryProvider<AppliedInterventionRepository>(
+              create: (context) => AppliedInterventionRepository())
         ],
         child: Builder(
-            builder: (context) => Container(
-                    child: Center(
-                  child: CustomIconButton(() async {
-                    print("querying");
-                    Survey survey =
-                        await SurveyRepository.getSurveyByID("test_survey");
-                    print("survey successfully loaded");
-                  }
-
-                      /*context
-                          .read<EntityRepository>()
-                          .getAllEntities()
-                          .then((value) => value.forEach((element) {
-                                print("entity: ${element.name}");
-                                print(element.level.interventionsAreAllowed
-                                    .toString());
-                                if (element.level.interventionsAreAllowed) {
-                                  print(element
-                                      .level.allowedInterventions!.length
-                                      .toString());
-                                  element.level.allowedInterventions!
-                                      .forEach((element) {
-                                    print(element.intervention.toString());
-                                  });
-                                }
-                              }))*/
-                      , FontAwesomeIcons.ad,
-                      Size(width(context) * .1, width(context) * .1), true),
-                ))));
+            builder: (context) => BlocProvider<OrganizationViewBloc>(
+                  create: (context) => OrganizationViewBloc(
+                      context.read<EntityRepository>(),
+                      context.read<AppliedInterventionRepository>(),
+                      context.read<InAppBloc>()),
+                  child:
+                      BlocBuilder<OrganizationViewBloc, OrganizationViewState>(
+                          builder: (context, state) {
+                    if (state is LoadingOrganizationViewState) {
+                      return Center(child: loadingSign(context));
+                    } else {
+                      return Column(
+                        children: [
+                          appBarWidget(context,
+                              state as EntitiesLoadedOrganizationViewState),
+                          mainWidget(context, state)
+                        ],
+                      );
+                    }
+                  }),
+                )));
   }
 }
