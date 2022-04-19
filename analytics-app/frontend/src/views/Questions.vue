2b3800579bc784431d85c5f675e9a35c5af36d8b
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-04-19 15:48:01 -0500

    updated questions view

--- analytics-app/frontend/src/views/Questions.vue
+++ analytics-app/frontend/src/views/Questions.vue
@@ -11,11 +11,14 @@
                 Fragen:
             </h2>
             <div class="questions">
-                <Question 
-                    v-for="question in questions"
-                    :key="question.id"
-                    :question="question"
-                />
+                <div 
+                    v-for="question in questions" 
+                    :key="question.id" 
+                    class="question"
+                    :class="{ 'active': selectedID === question.question_id }"
+                    @click="setActive(question)"> 
+                        {{ question.question_text }}
+                </div>
             </div>
         </div>
         <div class="answer-wrapper">
@@ -23,7 +26,10 @@
                 Antwort:
             </h2>
             <h3 style="text-align: left">
-                Ausgewählte Frage:
+                Ausgewählte Frage: {{  }}
+            </h3>
+            <h3 style="text-align: left">
+                Antwort: {{ selectedQuestion.answer }}
             </h3>
         </div>
       </div>
@@ -31,60 +37,58 @@
 </template>
 
 <script>
-import Question from '../components/surveys/Question.vue'
+import internal from 'stream';
+import { ref, reactive } from 'vue'
+
 export default {
     props: {},
-    components: { Question },
+    components: {},
+    setup: (questions, selectedQuestion) => {
+        var selectedID = ref();
+        var selectedQuestion = reactive([]);
+
+        const setActive = (question) => { selectedID.value = question.question_id; selectedQuestion = question;console.log(selectedQuestion.question_text)}
+        return {
+            selectedQuestion,
+            setActive,
+            selectedID,
+        };
+    },
     data() {
         return {
             questions: [
                 {
-                    question_id: '1',
+                    question_id: 1,
                     question_text: "Wie geht's?",
                     answer: 'gut',
                 },
                 {
-                    question_id: '1',
+                    question_id: 2,
                     question_text: "Trinkst du ausreichend Wasser?",
                     answer: 'Nein',
                 },
                 {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
+                    question_id: 3,
+                    question_text: "Machst du viel Sport?",
                     answer: 'Nein',
                 },
                 {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
+                    question_id: 4,
+                    question_text: "Kannst du schwimmen?",
                     answer: 'Nein',
                 },
                 {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
+                    question_id: 5,
+                    question_text: "Kannst du kochen?",
                     answer: 'Nein',
                 },
-                {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
-                    answer: 'Nein',
-                },
-                {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
-                    answer: 'Nein',
-                },
-                {
-                    question_id: '1',
-                    question_text: "Trinkst du ausreichend Wasser?",
-                    answer: 'Nein',
-                },
-            ]
+            ],
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
 
 .main {
     margin-left: 30px;
@@ -105,6 +109,26 @@ export default {
     border-right-style: solid;
     overflow: auto;
     max-height: 75vh;
+}
+
+.question {
+  width: 200px;
+  margin-left: 5px;
+  margin-right: 15px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  box-shadow: 0px 0px 1px rgb(0, 0, 0, 0.25); 
+
+  text-align: left;
+  padding-left: 5px;
+}
+
+.question:hover {
+  box-shadow: 0px 0px 5px rgb(0, 0, 0, 0.25); 
+}
+
+.question.active {
+  background-color: rgb(215, 215, 215);
 }
 
 .answer-wrapper {
