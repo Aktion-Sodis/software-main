57a24dd3186dddc3902fddddee865b662f69ebea
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-05-02 15:40:12 -0400

    tried some layout stuff in home view

--- analytics-app/frontend/src/views/Home.vue
+++ analytics-app/frontend/src/views/Home.vue
@@ -1,23 +1,247 @@
 <template>
-  <div>
-    <button @click="exampleButtonHandler">Example button</button>
-    <h2>{{ helloWorld }}</h2>
+  <div class="main">
+    <header class="header">
+      <div class="title"><h1>Test</h1></div>
+      <div class="sub-title"><h3>Test</h3></div>
+    </header>
+    <section class="body">
+      <div class="left">
+        <div class="left">
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+          <div class="question" :class="{ collapsed: true }">Test</div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="question-wrapper">Test</div>
+        <div class="answer-wrapper">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu
+          posuere ex. Suspendisse et tellus in tortor maximus feugiat maximus
+          sit amet mauris. In hac habitasse platea dictumst. Etiam gravida
+          interdum fermentum. Aliquam luctus id mauris eget egestas. Sed aliquet
+          eu risus quis mollis. Duis quis enim scelerisque nibh consectetur
+          tincidunt tempus quis quam. Curabitur eu iaculis lacus, ornare
+          vulputate mi. Nam aliquet nunc turpis, in finibus ligula vulputate in.
+          Nulla tortor purus, facilisis vitae bibendum quis, laoreet id nulla.
+          Duis posuere elementum odio. Sed eget scelerisque dui, nec vulputate
+          eros. Suspendisse potenti. Phasellus tellus lectus, blandit nec tellus
+          at, varius lobortis enim. Aenean lacinia tincidunt luctus. Cras congue
+          feugiat placerat. Aliquam erat volutpat. Maecenas non volutpat ex.
+          Aenean posuere ipsum lacinia posuere placerat. Proin id sapien mi.
+          Integer hendrerit tempor efficitur. Fusce tristique felis at maximus
+          ultrices. Nunc fermentum ante ut nisl porttitor, eleifend gravida erat
+          aliquam. Praesent et ante sit amet odio volutpat facilisis sed at
+          ipsum. Nulla porta vulputate turpis non vulputate. Sed neque dolor,
+          condimentum id nulla nec, bibendum lobortis ligula. Nulla vel aliquam
+          dui, non suscipit nibh. Ut et tincidunt augue, non lacinia turpis.
+          Nunc posuere quis leo sed auctor. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Fusce vitae tempor nisi. Morbi ultricies
+          neque et velit sollicitudin auctor. Donec a tellus auctor, pretium leo
+          id, accumsan risus. Proin blandit ipsum vitae rutrum accumsan. Donec
+          vehicula tellus sit amet dui semper tristique. In a elementum nunc, at
+          tempus ex. Vivamus elementum risus justo, at dictum purus laoreet ut.
+          Nulla at diam vitae tortor laoreet euismod nec sed diam. Fusce eu elit
+          eu urna condimentum semper. Pellentesque risus dui, vestibulum nec
+          dolor nec, suscipit dapibus felis. Nulla ac lorem non massa porta
+          placerat eget at lacus. Donec auctor, risus sit amet maximus
+          fringilla, leo ante faucibus eros, vel accumsan orci eros sit amet
+          est. Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Ut convallis risus non dictum suscipit. Ut
+          quam felis, viverra nec elit non, mattis molestie orci. Cras
+          ullamcorper erat et metus fringilla, eget ullamcorper est ultricies.
+          Praesent sit amet rutrum justo. Cras vitae dui lectus. Integer
+          accumsan quis velit sit amet sodales. Vestibulum a consequat urna.
+          Duis sed urna in enim ornare aliquet. Maecenas pellentesque ipsum
+          elit, at cursus neque eleifend vel. Duis in turpis non mauris vehicula
+          pharetra. Suspendisse sodales lorem ac rutrum euismod. Nunc elementum
+          vulputate eros eget sollicitudin. In ultrices, nibh a tempus
+          porttitor, tortor ligula auctor sapien, et suscipit felis dui sit amet
+          ligula. Pellentesque scelerisque eleifend est quis tincidunt.
+          Vestibulum id nunc sed eros euismod tincidunt eu sit amet nisi. Sed id
+          elit quam. Donec eget nisl a neque sagittis posuere. Ut dignissim elit
+          quis est imperdiet, quis elementum enim consectetur. Suspendisse
+          potenti. Integer gravida justo metus, eu aliquet tortor imperdiet nec.
+          Donec mattis at ante quis eleifend. Morbi sed nibh nulla. Etiam
+          aliquet sapien lobortis aliquet semper. Morbi ac dui sed orci
+          fermentum auctor in vitae est. Proin tincidunt magna vel mauris
+          posuere ultrices. Ut nec odio odio. In ac lectus a mauris hendrerit
+          fringilla ut quis elit. Sed ut risus vestibulum, congue sem a,
+          faucibus odio. Mauris vitae ultrices eros. Proin arcu quam, ultricies
+          quis dictum vel, tristique et mauris. Nam sed molestie nisl.
+          Pellentesque ultrices accumsan vulputate. Donec id blandit purus.
+          Maecenas eget orci mattis, sagittis nunc vitae, rhoncus augue. Donec
+          id luctus purus. Morbi lacinia facilisis dolor, vitae posuere mauris
+          cursus in. Cras consectetur, nisl luctus sodales scelerisque, ante
+          justo imperdiet lectus, quis rhoncus nibh neque nec nulla. Mauris arcu
+          turpis, lacinia sit amet mattis non, pharetra vitae mi. Nullam
+          faucibus luctus magna vitae cursus. Nam risus libero, laoreet et
+          efficitur et, condimentum ut dui. Nullam bibendum neque in arcu
+          lobortis accumsan. Sed dictum, mi sed iaculis ullamcorper, ligula nisi
+          maximus est, ut fermentum neque eros sit amet nulla. Phasellus cursus
+          facilisis odio, vel dapibus neque ultrices sed. Maecenas a fermentum
+          nisl, a viverra enim. Duis consequat, odio non rutrum ultrices, erat
+          arcu mollis eros, in maximus elit ipsum non nibh. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Aenean consectetur sem vel dictum dignissim. Morbi cursus magna sem,
+          et euismod quam finibus vel. Fusce vel sem neque. Aenean accumsan ex
+          ante, a mattis urna finibus a. Nunc gravida nisl id mi mattis
+          suscipit. Sed fermentum lacus nunc, ut eleifend nibh facilisis sed.
+          Cras finibus efficitur ornare. Pellentesque suscipit neque nec rhoncus
+          molestie. Curabitur feugiat tristique blandit. Sed vitae augue eu
+          neque suscipit eleifend in sit amet velit. Etiam in orci ac ipsum
+          imperdiet dapibus a eu diam. Aliquam sagittis eu neque quis tempor.
+          Nulla in elit lobortis, tincidunt ex eget, suscipit turpis. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu posuere
+          ex. Suspendisse et tellus in tortor maximus feugiat maximus sit amet
+          mauris. In hac habitasse platea dictumst. Etiam gravida interdum
+          fermentum. Aliquam luctus id mauris eget egestas. Sed aliquet eu risus
+          quis mollis. Duis quis enim scelerisque nibh consectetur tincidunt
+          tempus quis quam. Curabitur eu iaculis lacus, ornare vulputate mi. Nam
+          aliquet nunc turpis, in finibus ligula vulputate in. Nulla tortor
+          purus, facilisis vitae bibendum quis, laoreet id nulla. Duis posuere
+          elementum odio. Sed eget scelerisque dui, nec vulputate eros.
+          Suspendisse potenti. Phasellus tellus lectus, blandit nec tellus at,
+          varius lobortis enim. Aenean lacinia tincidunt luctus. Cras congue
+          feugiat placerat. Aliquam erat volutpat. Maecenas non volutpat ex.
+          Aenean posuere ipsum lacinia posuere placerat. Proin id sapien mi.
+          Integer hendrerit tempor efficitur. Fusce tristique felis at maximus
+          ultrices. Nunc fermentum ante ut nisl porttitor, eleifend gravida erat
+          aliquam. Praesent et ante sit amet odio volutpat facilisis sed at
+          ipsum. Nulla porta vulputate turpis non vulputate. Sed neque dolor,
+          condimentum id nulla nec, bibendum lobortis ligula. Nulla vel aliquam
+          dui, non suscipit nibh. Ut et tincidunt augue, non lacinia turpis.
+          Nunc posuere quis leo sed auctor. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Fusce vitae tempor nisi. Morbi ultricies
+          neque et velit sollicitudin auctor. Donec a tellus auctor, pretium leo
+          id, accumsan risus. Proin blandit ipsum vitae rutrum accumsan. Donec
+          vehicula tellus sit amet dui semper tristique. In a elementum nunc, at
+          tempus ex. Vivamus elementum risus justo, at dictum purus laoreet ut.
+          Nulla at diam vitae tortor laoreet euismod nec sed diam. Fusce eu elit
+          eu urna condimentum semper. Pellentesque risus dui, vestibulum nec
+          dolor nec, suscipit dapibus felis. Nulla ac lorem non massa porta
+          placerat eget at lacus. Donec auctor, risus sit amet maximus
+          fringilla, leo ante faucibus eros, vel accumsan orci eros sit amet
+          est. Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Ut convallis risus non dictum suscipit. Ut
+          quam felis, viverra nec elit non, mattis molestie orci. Cras
+          ullamcorper erat et metus fringilla, eget ullamcorper est ultricies.
+          Praesent sit amet rutrum justo. Cras vitae dui lectus. Integer
+          accumsan quis velit sit amet sodales. Vestibulum a consequat urna.
+          Duis sed urna in enim ornare aliquet. Maecenas pellentesque ipsum
+          elit, at cursus neque eleifend vel. Duis in turpis non mauris vehicula
+          pharetra. Suspendisse sodales lorem ac rutrum euismod. Nunc elementum
+          vulputate eros eget sollicitudin. In ultrices, nibh a tempus
+          porttitor, tortor ligula auctor sapien, et suscipit felis dui sit amet
+          ligula. Pellentesque scelerisque eleifend est quis tincidunt.
+          Vestibulum id nunc sed eros euismod tincidunt eu sit amet nisi. Sed id
+          elit quam. Donec eget nisl a neque sagittis posuere. Ut dignissim elit
+          quis est imperdiet, quis elementum enim consectetur. Suspendisse
+          potenti. Integer gravida justo metus, eu aliquet tortor imperdiet nec.
+          Donec mattis at ante quis eleifend. Morbi sed nibh nulla. Etiam
+          aliquet sapien lobortis aliquet semper. Morbi ac dui sed orci
+          fermentum auctor in vitae est. Proin tincidunt magna vel mauris
+          posuere ultrices. Ut nec odio odio. In ac lectus a mauris hendrerit
+          fringilla ut quis elit. Sed ut risus vestibulum, congue sem a,
+          faucibus odio. Mauris vitae ultrices eros. Proin arcu quam, ultricies
+          quis dictum vel, tristique et mauris. Nam sed molestie nisl.
+          Pellentesque ultrices accumsan vulputate. Donec id blandit purus.
+          Maecenas eget orci mattis, sagittis nunc vitae, rhoncus augue. Donec
+          id luctus purus. Morbi lacinia facilisis dolor, vitae posuere mauris
+          cursus in. Cras consectetur, nisl luctus sodales scelerisque, ante
+          justo imperdiet lectus, quis rhoncus nibh neque nec nulla. Mauris arcu
+          turpis, lacinia sit amet mattis non, pharetra vitae mi. Nullam
+          faucibus luctus magna vitae cursus. Nam risus libero, laoreet et
+          efficitur et, condimentum ut dui. Nullam bibendum neque in arcu
+          lobortis accumsan. Sed dictum, mi sed iaculis ullamcorper, ligula nisi
+          maximus est, ut fermentum neque eros sit amet nulla. Phasellus cursus
+          facilisis odio, vel dapibus neque ultrices sed. Maecenas a fermentum
+          nisl, a viverra enim. Duis consequat, odio non rutrum ultrices, erat
+          arcu mollis eros, in maximus elit ipsum non nibh. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Aenean consectetur sem vel dictum dignissim. Morbi cursus magna sem,
+          et euismod quam finibus vel. Fusce vel sem neque. Aenean accumsan ex
+          ante, a mattis urna finibus a. Nunc gravida nisl id mi mattis
+          suscipit. Sed fermentum lacus nunc, ut eleifend nibh facilisis sed.
+          Cras finibus efficitur ornare. Pellentesque suscipit neque nec rhoncus
+          molestie. Curabitur feugiat tristique blandit. Sed vitae augue eu
+          neque suscipit eleifend in sit amet velit. Etiam in orci ac ipsum
+          imperdiet dapibus a eu diam. Aliquam sagittis eu neque quis tempor.
+          Nulla in elit lobortis, tincidunt ex eget, suscipit turpis.
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-export default {
-  name: "Home",
-  computed: {
-    ...mapGetters({
-      helloWorld: "getHelloWorld",
-    }),
-  },
-  methods: {
-    ...mapActions({
-      exampleButtonHandler: "exampleButtonHandler",
-    }),
-  },
-};
+export default {};
 </script>
+
+<style scoped>
+.main {
+  height: 100%;
+  background-color: rgb(243, 0, 0);
+  position: absolute;
+}
+.header {
+  position: sticky;
+  top: 0;
+}
+.title {
+  background-color: rgb(41, 243, 0);
+}
+.sub-title {
+  background-color: rgb(0, 93, 243);
+}
+.body {
+  display: flex;
+  justify-content: space-between;
+  height: 90%;
+  position: relative;
+}
+.left {
+  width: 5%;
+  background-color: rgb(96, 61, 75);
+  overflow: scroll;
+}
+.question {
+  min-height: 70px;
+  margin-left: 5px;
+  margin-right: 15px;
+  margin-bottom: 10px;
+  margin-top: 2px;
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 0px 1px rgb(0, 0, 0, 0.25);
+  border-radius: 5px;
+
+  text-align: left;
+  padding-left: 5px;
+
+  background-color: #2d91be;
+  color: white;
+}
+.right {
+  background-color: rgb(228, 135, 174);
+  width: 95%;
+  bottom: 0;
+}
+.question-wrapper {
+  height: 5%;
+}
+.answer-wrapper {
+  height: 85vh;
+  overflow: scroll;
+  bottom: 0;
+}
+.footer {
+  height: 1px;
+  background-color: rgb(12, 9, 9);
+}
+</style>
